16da7057dae05e9547d7f4842988ec7eee16c1eb
navinvincent <84716123+navinvincent@users.noreply.github.com>
2021-12-22 11:51:22 +0530

    Update Dynamic_plots.m

--- FIgure 1/B/Dynamic_plots.m
+++ FIgure 1/B/Dynamic_plots.m
@@ -53,60 +53,19 @@ disp("High + Low= "+combine+', Hybrid= '+inter);
 %% Adding feature to the plot:
 % gcf % get current figure:
 xlim([0 100]);
-ylim([0 3.5]);
+ylim([0 1]);
 xlabel('Time (a.u)',"FontSize",12,"FontName",'Arial','FontWeight','bold');
 ylabel('Concentration A(a.u)',"FontSize",12,"FontName",'Arial','FontWeight','bold');
 
-str = {
-'g_A = 5',   
-'g_B = 5',
-'\gamma_A = 0.1',
-'\gamma_B = 0.1',  
-'A_0B = 120',
-'B_0A = 120', 
-'A_0A = 80',   
-'B_0B = 80',
-'n_{AB} = 1',
-'n_{BA} = 1',
-'n_{AA} = 4',   
-'n_{BB} = 4',
-'\lambda_{AB} = 0.1',
-'\lambda_{BA} = 0.1',
-'\lambda_{AA} = 5',  
-'\lambda_{BB} = 5'};
 
-text(900,130,str,'FontSize',8);
-% 
-% saveas(gcf,'toggle_w_selfAct_4.png')
 
 hold off 
 
-for i=1:1:50
-    A = 10*rand(1); 
-    B = 10*rand(1); 
-    %% In general, you can generate N random numbers in the 
-    %% interval (a,b) with the formula r = a + (b-a).*rand(N,1).
-    
-%% Calling ODE function:
-
-    [t, x] = ode45(@(t,x) interactions(t,x),domain,[A;B]);
-    %Ploting:
-    plot(t,x(:,2), 'LineWidth', 2, 'DisplayName', 'B'); 
-    %plot(x(:,1),x(:,2)); 
-    hold on;
-    %plot(t,x(:,2), 'LineWidth', 2, 'DisplayName', 'B');
-    %hold on
-    
-end
-xlim([0 500]);
-ylim([0 20]);
-xlabel('Time (a.u)',"FontSize",12,"FontName",'Arial','FontWeight','bold');
-ylabel('Concentration B (a.u)',"FontSize",12,"FontName",'Arial','FontWeight','bold');
 
 
-
-Defining the Local functions used for the plotting
+%Defining the Local functions used for the plotting
 % hill function
+
 
 function H = hill(X,X0,lambda,n)
 % H+ 
@@ -134,12 +93,12 @@ g_A = 10;
 g_B = 10;   
 
 % Hills function threshold :
-A0B = 10;
-B0A = 10;
+A0B = 2;
+B0A = 2;
 A0A = 10;   B0B = 10;
 
 % Cooperativity/ hill function coefficient:
-nAtoB = 2;   nBtoA = 2;
+nAtoB = 5;   nBtoA = 5;
 nAtoA = 3;   nBtoB = 3;
 
 % fold change/ lambda
@@ -147,10 +106,9 @@ lambda_AtoB = 0.1;   lambda_BtoA = 0.1;
 lambda_AtoA = 3;   lambda_BtoB = 3;
 
 % %% equations:
-dxdt(1) = g_A*hill(x(1),A0A,lambda_AtoA,nAtoA)*hill(x(2),B0A,lambda_BtoA,nBtoA) - gamma_A*x(1);  %% equation A
-dxdt(2) = g_B*hill(x(2),B0B,lambda_BtoB,nBtoB)*hill(x(1),A0B,lambda_AtoB,nAtoB) - gamma_B*x(2); % equation B
-end
+
 % % equations:
-% dxdt(1) = g_A*hill(x(2),B0A,lambda_BtoA,nBtoA) - gamma_A*x(1) ;  %% equation A
-% dxdt(2) = g_B*hill(x(1),A0B,lambda_AtoB,nAtoB) - gamma_B*x(2) ; % equation B
-% end
+dxdt(1) = g_A*hill(x(2),B0A,lambda_BtoA,nBtoA) - gamma_A*x(1) ;  %% equation A
+dxdt(2) = g_B*hill(x(1),A0B,lambda_AtoB,nAtoB) - gamma_B*x(2) ; % equation B
+ end
+
